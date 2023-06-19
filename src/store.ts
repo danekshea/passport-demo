2ed0756dc8f55509c9d80b9773c200fcf10a2270
danekshea <danekshea@gmail.com>
2023-06-19 21:27:39 +1000

    production ready

--- src/store.ts
+++ src/store.ts
@@ -5,9 +5,9 @@ const passportConfig = {
 	baseConfig: new ImmutableConfiguration({
 		environment: Environment.SANDBOX
 	}),
-	clientId: 'dc4a9kyam6B8IGrA36ZKZRs39ESdZpM4', // import.meta.env.VITE_CLIENT_ID,
-	redirectUri: 'http://127.0.0.1:8080/login', //"https://stately-gingersnap-077133.netlify.app/login", // import.meta.env.VITE_REDIRECT_URL,
-	logoutRedirectUri: 'http://127.0.0.1', //"https://stately-gingersnap-077133.netlify.app", //import.meta.env.VITE_LOGOUT_URL,
+	clientId: 'vp7iWfggWyIZe4BvQjk3SHCESdtlmEOk', // import.meta.env.VITE_CLIENT_ID,
+	redirectUri: 'https://passport-demo.vercel.app/login', //"https://stately-gingersnap-077133.netlify.app/login", // import.meta.env.VITE_REDIRECT_URL,
+	logoutRedirectUri: 'https://passport-demo.vercel.app/', //"https://stately-gingersnap-077133.netlify.app", //import.meta.env.VITE_LOGOUT_URL,
 	audience: 'platform_api',
 	scope: 'openid offline_access email transact'
 };
